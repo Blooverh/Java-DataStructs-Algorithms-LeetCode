61094b3379bc7ad20bc572acc4044dbc728622ff
Diogo Aires <espadinsky@gmail.com>
2023-03-02 14:43:56 -0600

    Update Heap.java

--- DataAndAlgoL/Chpt7BinaryHeapsPriorityQs/Heap.java
+++ DataAndAlgoL/Chpt7BinaryHeapsPriorityQs/Heap.java
@@ -79,4 +79,109 @@ public class Heap {
 
         percolateDown(max); //recursive call untill all nodes are correct location.
     }
+
+    //Deleting an element from the heap, we only need to delete the element from the root 
+    /*
+     * After deleting the root element, copy the last element of the heap and delete that last element.
+     * After replacing the last element, tree may not satisfy heap property and hence we need to make it a heap again
+     * by calling percolate down function
+     */
+
+     int DeleteMax(){
+        if(this.count==0){
+            return -1;
+        }
+
+        int data= this.array[0];
+        this.array[0]= this.array[this.count-1];
+        this.count--; //reduces heap size
+        percolateDown(0);
+        return data;
+    }
+
+    //Inserting an element is simliar to heapifying and deleting, but instead of percolating down we use percolate up function
+    /*
+     * increase the heap size
+     * keep new element at end of the heap
+     * heapify the element from bottom to top (root)
+     */
+    public void Insert(int data){
+        int i;
+        if(this.count ==this.capacity){
+            resizeHeap();
+        }
+
+        this.count++; //increasing the heap size to hold this new item 
+        i= this.count-1;
+
+        while(i>=0 && data > this.array[(i-2)/2]){
+            this.array[i] =this.array[(i-1)/2];
+            i= i-1/2;
+        }
+
+        this.array[i]=data;
+    }
+
+    public void resizeHeap(){
+        int[] array_old= new int[this.capacity];
+        System.arraycopy(this.array,0,array_old, this.count-1, capacity);
+        this.array= new int[this.capacity*2];
+        if(this.array== null){
+            System.out.println("Memory error");
+            return;
+        }
+
+        for(int i=0; i< this.capacity; i++){
+            this.array[i]=array_old[i];
+        }
+
+        this.capacity*=2;
+        array_old=null;
+    }
+
+    public void DestroyHeap(){
+        this.count=0;
+        this.array=null;
+    }
+
+    //in case we already have an array we have to heapify the array so it can become a heap
+    /*
+     * 
+     */
+
+     public void BuildHeap(Heap h, int[] A, int n){
+        if(h== null){
+            return;
+        }
+
+        while(n > this.capacity){
+            h.resizeHeap();
+        }
+
+        for(int i=0; i< n; i++){
+            h.array[i] = A[i];
+        }
+
+        this.count=n;
+        for(int i= (n-1)/2; i >=0; i--){
+            h.percolateDown(i);
+        }
+     }
+
+
+     //HEAP SORT -> INSERTS ALL ELEMENTS FROM UNSORTED ARRAY INTO HEAP, THEN REMOVES THE ROOT ELEMENTS UNTIL THE HEAP IS EMPTY
+     public void heapSort(int[] A, int n){
+        Heap h= new Heap(n,0);
+        int old_size= h.count;
+        int temp;
+
+        for(int i= n-1; i>0; i--){ //h.array[0] is the largest element
+            temp=h.array[0];
+            h.array[0]= h.array[h.count-1];
+            h.count--;
+            h.percolateDown(0);
+        }
+
+        h.count= old_size;
+     }
 }
