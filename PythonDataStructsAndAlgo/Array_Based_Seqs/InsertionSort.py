4273e091e313779b32667ea7af19bd37e0c96c55
Diogo Aires <63264160+Blooverh@users.noreply.github.com>
2023-04-26 22:47:49 -0500

    Update InsertionSort.py

--- PythonDataStructsAndAlgo/Array_Based_Seqs/InsertionSort.py
+++ PythonDataStructsAndAlgo/Array_Based_Seqs/InsertionSort.py
@@ -1,3 +1,5 @@
+"""Insertion sort runs at O(n^2) due to the nested loops
+for an unordered list."""
 def insertion_Sort(A):
     """Sort list of comparable elements into ascending order"""
 
@@ -7,8 +9,9 @@ def insertion_Sort(A):
         #while A[j-1] > than curr (A[j]) we swap leftwards until A[j-1] <curr
         while j >0 and A[j-1]>curr: #element A[j-1] must be after current
             A[j]=A[j-1]
-            j-=1
-        A[j]=curr #current is now in the right place
+            j-=1 #current index that was swapped that will hold the current element we used for comparison 
+        A[j]=curr #current goes now to the right index
+        #And we iterate the loop with again with the next kth element.
     
     return A
 
